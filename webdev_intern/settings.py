b4777072e92595d48a4033d34b61262969247256
frwkHoangQuy <frwkHoangQuy@gmail.com>
2024-12-27 11:13:39 +0700

    update setting mysql

--- webdev_intern/settings.py
+++ webdev_intern/settings.py
@@ -30,8 +30,12 @@ config.read(os.path.join(BASE_DIR, 'config.ini'))
 # Cấu hình database từ tệp config.ini
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': config['mysql']['database'],
+        'USER': config['mysql']['user'],
+        'PASSWORD': config['mysql']['password'],
+        'HOST': config['mysql']['host'],
+        'PORT': config['mysql']['port'],
     }
 }
 
